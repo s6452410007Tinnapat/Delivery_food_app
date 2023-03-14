86604772fdba92bbaa71034fb33ba326bae26f1f
s6452410007Tinnapat <s6452410007@sau.ac.th>
2023-03-14 16:33:10 +0700

    v.1 by Pon : create project

---
+++ lib/models/FoodShopList.dart
@@ -0,0 +1,19 @@
+class FoodShopList{
+  String shopImage = '';
+  String shopName = '';
+  String shopPhone = '';
+  String shopWeb = '';
+  String shopFacebook = '';
+  String shopLatitude = '';
+  String shopLongitude = '';
+
+  FoodShopList({
+    required this.shopImage,
+    required this.shopName,
+    required this.shopPhone,
+    required this.shopWeb,
+    required this.shopFacebook,
+    required this.shopLatitude,
+    required this.shopLongitude,
+  });
+}
